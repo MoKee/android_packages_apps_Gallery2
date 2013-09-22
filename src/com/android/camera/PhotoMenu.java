95e5dd4f8cbcad46c9afc4be808dcf8143c1a035
martincz <martincz@mokeetek.com>
2013-09-23 01:49:31 +0800

    fix merge
    Change-Id: Ie6ac6cf34d57fb95bd6ac594297517b43ec06cb2

--- src/com/android/camera/PhotoMenu.java
+++ src/com/android/camera/PhotoMenu.java
@@ -173,20 +173,21 @@ public class PhotoMenu extends PieController
         final ListPreference sizePref = group.findPreference(CameraSettings.KEY_PICTURE_SIZE);
         if (sizePref != null) {
             item = makeItem(R.drawable.ic_imagesize);
-        item.setLabel(res.getString(R.string.pref_camera_picturesize_title).toUpperCase(locale));
-        item.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(PieItem item) {
-                ListPrefSettingPopup popup = (ListPrefSettingPopup) mActivity.getLayoutInflater().inflate(
-                        R.layout.list_pref_setting_popup, null, false);
-                popup.initialize(sizePref);
-                popup.setSettingChangedListener(PhotoMenu.this);
-                mUI.dismissPopup();
-                mPopup = popup;
-                mUI.showPopup(mPopup);
-            }
-        });
-        settings.addItem(item);
+            item.setLabel(res.getString(R.string.pref_camera_picturesize_title).toUpperCase(locale));
+            item.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(PieItem item) {
+                    ListPrefSettingPopup popup = (ListPrefSettingPopup) mActivity.getLayoutInflater().inflate(
+                            R.layout.list_pref_setting_popup, null, false);
+                    popup.initialize(sizePref);
+                    popup.setSettingChangedListener(PhotoMenu.this);
+                    mUI.dismissPopup();
+                    mPopup = popup;
+                    mUI.showPopup(mPopup);
+                }
+            });		
+            settings.addItem(item);
+        }
         // jpeg quality
         if (group.findPreference(CameraSettings.KEY_CAMERA_JPEG) != null) {
             item = makeItem(R.drawable.ic_jpeg);
